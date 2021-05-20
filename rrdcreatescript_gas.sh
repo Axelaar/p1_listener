6afc18155ac73efa8cc80c14a7037631d5d4a898
Bram <44711741+Axelaar@users.noreply.github.com>
2021-05-20 21:37:36 +0200

    Add files via upload

---
+++ rrdcreatescript_gas.sh
@@ -0,0 +1,12 @@
+rrdtool create /home/logger/data/gas.rrd --step 3600   \
+DS:Gas:COUNTER:10000:0:100000000   \
+RRA:MIN:0.5:6:1460            \
+RRA:MIN:0.5:24:3650            \
+RRA:MIN:0.5:168:1300            \
+RRA:AVERAGE:0.5:1:744            \
+RRA:AVERAGE:0.5:6:1460            \
+RRA:AVERAGE:0.5:24:3650            \
+RRA:AVERAGE:0.5:168:1300            \
+RRA:MAX:0.5:6:1460            \
+RRA:MAX:0.5:24:3650            \
+RRA:MAX:0.5:168:1300           
