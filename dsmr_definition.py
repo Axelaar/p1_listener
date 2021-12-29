206b437f4efdca72130f1eee36c9dcaa824b7b2e
Bram <44711741+Axelaar@users.noreply.github.com>
2021-12-29 16:31:35 +0100

    Update dsmr_definition.py
    Complete overhaul

--- dsmr_definition.py
+++ dsmr_definition.py
@@ -1,78 +1,88 @@
 #!/usr/bin/python3
 #
-# Analyze the standard P1 telegram to define 
-# length of required datafields.
+# BEFORE RUNNING THIS 'dsmr_definition.py' SCRIPT:
+# Generate a text file with a complete single P1 telegram in
+# directory /home/pi/dsmr/ by executing 'p1_telegram_discovery.py'.
+# Default text file name is 'standard_telegram.txt'.
+#
+# Script 'dsmr_definition.py' analyzes the standard p1 telegram 
+# to define the length of required datafields.
 # Run this script to generate the telegram definitions
 # to be used by the 'p1_listener.py' script.
 #
-# Place a text file with a complete single P1 telegram
-# in directory /home/pi/dsmr/.
-# Default text file name is 'standard_telegram.txt'.
-# Generate this file by executing 'p1_telegram_discovery.py'.
-#
 # This script evaluates only the obis codes that are
-# included in the obis code listing below.
-# The obis code is the key, the value is the variable
+# included in the obis code listing specified in the config file.
+# The value shown with each OBIS code is the variable
 # name to be used in the 'p1_listener.py' script.
 #
+# Configuration settings are taken from /home/pi/dsmr/dsmr_definition.conf.
+#
 # Created by Bram Langen - Mar 5, 2020.
-# v1.41 - Corrected the dsmr directory path.
+# v1.50 - Modified to work with a config file.
 
-import os;
-import re;
-import sys;
+from    colorama                   import Fore, Style
+from    configparser               import ConfigParser;
+import  os;
+from    re                         import sub as substitute;
+import  sys;
+# Add the local library to the default path listing.
+sys.path.insert(0, '/home/pi/local_lib');
+from    local_lib.custom_functions import *;
+
+############################################################
+# Obtain the config file name based on the
+# name of the script.
+# sys.argv[0] is the script path and file name
+############################################################
+path, logfile, configfile = generate_filenames(sys.argv[0]);
+if path == '':
+    # obtain the path if the script is run locally
+    import pathlib;
+    path = str(pathlib.Path(__file__).parent.resolve()) + '/';
 
 ############################################################
 # Initialize / declare variables
 ############################################################
-filename_in             = '/home/pi/dsmr/standard_telegram.txt';
-filename_out            = '/home/pi/dsmr/telegram_definition.txt';
-obis_count              = 0;
+obis_count      = 0;
+object_list     = [];
+obis            = 0;
+long_text       = 1;
+IP_id           = 2;
+ws_id           = 3;
+
 
 ############################################################
-# Define data_object class.
-# This class is used to link the obis code to the variable
-# name in plain text as it is used in 'p1_listener.py'.
-# The last two fields are left blank, they are reserved for
-# the websocket identifier and the IP Telegram identifier.
+# Obtain the variables from the configuration file.
+# Create object_list tuple: obis, long_text, IP_id, ws_id
 ############################################################
-class data_object:
-    def __init__(self, obis, long_text, IP_id, ws_id):
-        self.obis       = obis;
-        self.long_text  = long_text;
-        self.IP_id      = IP_id;
-        self.ws_id      = ws_id;
+print(f'Parsing configuration file {configfile}.');
+
+if os.path.isfile(configfile):
+    parser = ConfigParser();
+    parser.read(configfile);
+    if 'path' in parser['path']:
+        path            = parser['path']['path'];
+    standard_telegram   = parser['files']['standard telegram'];
+    object_definitions  = parser['files']['object definitions'];
+    if 'log' in parser['files']:
+        logfile         = parser['files']['log'];
+    if 'target' in parser['timezone']:
+        target_timezone = parser['timezone']['target'];
+    for key in parser['obis']:
+        # combine the key name with the key data...
+        obis_data       = parser['obis'][key] + ' ' + key + ' ' + ' ';
+        # ... convert the data to a tuple ...
+        obis_data       =   obis_data.split(' ');
+        # ... and append this dataset to the list
+        object_list.append(obis_data);
+else:
+    print('a', f'Config file {configfile} not found.');
+    exit();
 
 ############################################################
-# Populate the desired obis codes listing.
-# Comment out the obis code lines that you do not want
-# to monitor.
-# The long text value represents the variable name as used 
-# in the monitoring script.
-#
-#                **********IMPORTANT:**********
-# '1-3:0.2.8': 'dsmr_version' is a required entry.
-#
-############################################################
-object_list = [];# This is a required statement.
-object_list.append(data_object('1-3:0.2.8', 'dsmr_version', '', ''));# This is a required entry.
-object_list.append(data_object('1-0:1.8.1', 'kwh_total_in_tariff1', '', ''));
-object_list.append(data_object('1-0:1.8.2', 'kwh_total_in_tariff2', '', ''));
-object_list.append(data_object('1-0:2.8.1', 'kwh_total_out_tariff1', '', ''));
-object_list.append(data_object('1-0:2.8.2', 'kwh_total_out_tariff2', '', ''));
-object_list.append(data_object('1-0:1.7.0', 'kw_actual_in', '', ''));
-object_list.append(data_object('1-0:2.7.0', 'kw_actual_out', '', ''));
-object_list.append(data_object('0-0:96.14.0', 'tariff', '', ''));
-object_list.append(data_object('0-1:24.2.1', 'gas', '', ''));
-object_list.append(data_object('1-0:21.7.0', 'kw_actual_in_A', '', ''));
-object_list.append(data_object('1-0:41.7.0', 'kw_actual_in_B', '', ''));
-object_list.append(data_object('1-0:61.7.0', 'kw_actual_in_C', '', ''));
-object_list.append(data_object('1-0:22.7.0', 'kw_actual_out_A', '', ''));
-object_list.append(data_object('1-0:42.7.0', 'kw_actual_out_B', '', ''));
-object_list.append(data_object('1-0:62.7.0', 'kw_actual_out_C', '', ''));
-
-############################################################
-# Verify directory dsmr exists
+# Verify directory dsmr exists, and create it if it doesn't
+# (Normally not needed, as this script should reside in
+# the dsmr directory.
 ############################################################
 if not os.path.isdir('/home/pi/dsmr'):
     os.mkdir('home/pi/dsmr');
@@ -84,36 +94,35 @@ if not os.path.isdir('/home/pi/dsmr'):
 # entry in the definitions file with the specifics for
 # each data object.
 ############################################################
-if os.path.isfile(filename_in):
-    print (filename_in , 'found.');
-    f_read  = open(filename_in, 'r');
-    f_write = open(filename_out, 'w');
+if os.path.isfile(standard_telegram):
+    print (f'Now reading file {Style.BRIGHT}{standard_telegram}{Style.RESET_ALL}');
+    f_read  = open(standard_telegram, 'r');
+    print (f'and writing file {Style.BRIGHT}{object_definitions}{Style.RESET_ALL}.');
+    f_write = open(object_definitions, 'w');
     f_write.write('# This file is generated by ' + sys.argv[0] + '\r\n');
     while True:
         input_line = f_read.readline();
         if input_line[0:1] == '!':
             # The last line of the telegram is the CRC value, preceded by an exclamation mark.
             break
-        # enumerate returns a tuple where the first value is the index number, and the second
-        # value the original value of the list.
-        for index, object in enumerate(object_list):
-            if object.obis in input_line:
+        for object in object_list:
+            if object[0] in input_line:
                 if input_line[2] == '0':
                     # The channel number is '0' for the electricity meter, and a higher number for the gas meter.
-                    stripped_line = re.sub(r'[a-zA-Z*]', '', input_line);
+                    stripped_line = substitute(r'[a-zA-Z*]', '', input_line);
                     starting_position = stripped_line.find('(') +1;
                     ending_position = stripped_line.find(')');
                 else:
                     stripped_line = input_line.replace('*m3', '');
                     starting_position = stripped_line.find('(') +1;
                     ending_position = stripped_line.find(')', -2);     # search for the ending bracket only.
-                f_write.write(object.obis + ';' + str(starting_position) + ';' + \
-                    str(ending_position) + ';' + object.long_text + ';' + \
-                    object.IP_id + ';' + object.ws_id + ';\r\n');
+                f_write.write(object[obis] + ';' + str(starting_position) + ';' + \
+                    str(ending_position) + ';' + object[long_text] + ';' + \
+                    object[IP_id] + ';' + object[ws_id] + ';\r\n');
                 obis_count += 1;
     f_read.close();
     f_write.close();
-    print ('Script completed succesfully, converted %d obis codes.' % obis_count);
+    print (f'Script completed {Fore.GREEN}{Style.BRIGHT}succesfully{Style.RESET_ALL}, converted {Style.BRIGHT}{obis_count}{Style.NORMAL} obis codes.');
 else:
-    print ('Error: ' + filename_in + ' not found');
+    print (f'Error: file {standard_telegram} not found');
 exit()
